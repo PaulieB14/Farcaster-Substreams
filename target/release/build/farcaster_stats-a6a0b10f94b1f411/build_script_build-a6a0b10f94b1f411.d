bc6fa6a0ef521dcb05e0d4f34ca9874e33e23d83
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-08-07 14:18:19 -0400

    Update to v1.0.2 with correct GitHub repository URL

---
+++ target/release/build/farcaster_stats-a6a0b10f94b1f411/build_script_build-a6a0b10f94b1f411.d
@@ -0,0 +1,5 @@
+/Users/paulbarba/farcaster-substream/farcaster-stats/target/release/build/farcaster_stats-a6a0b10f94b1f411/build_script_build-a6a0b10f94b1f411.d: build.rs
+
+/Users/paulbarba/farcaster-substream/farcaster-stats/target/release/build/farcaster_stats-a6a0b10f94b1f411/build_script_build-a6a0b10f94b1f411: build.rs
+
+build.rs:
--- target/wasm32-unknown-unknown/release/deps/farcaster_stats.d
+++ target/wasm32-unknown-unknown/release/deps/farcaster_stats.d
@@ -1,8 +1,8 @@
-/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/deps/farcaster_stats.d: src/lib.rs /Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-ffc12e9a653dc350/out/farcaster_stats.v1.rs
+/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/deps/farcaster_stats.d: src/lib.rs /Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-d60187b82457979a/out/farcaster_stats.v1.rs
 
-/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/deps/farcaster_stats.wasm: src/lib.rs /Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-ffc12e9a653dc350/out/farcaster_stats.v1.rs
+/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/deps/farcaster_stats.wasm: src/lib.rs /Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-d60187b82457979a/out/farcaster_stats.v1.rs
 
 src/lib.rs:
-/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-ffc12e9a653dc350/out/farcaster_stats.v1.rs:
+/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-d60187b82457979a/out/farcaster_stats.v1.rs:
 
-# env-dep:OUT_DIR=/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-ffc12e9a653dc350/out
+# env-dep:OUT_DIR=/Users/paulbarba/farcaster-substream/farcaster-stats/target/wasm32-unknown-unknown/release/build/farcaster_stats-d60187b82457979a/out
